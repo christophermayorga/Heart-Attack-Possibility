0efed0506fcc87003c76a34d2ad09a46b2369aad
Christopher Mayorga <christopher.mayorga2@gmail.com>
2021-10-04 13:37:35 -0500

    Add imports

--- model.py
+++ model.py
@@ -1,3 +1,30 @@
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer  
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
+from sklearn.model_selection import train_test_split 
+from sklearn.tree import DecisionTreeClassifier
+from pprint import pprint 
+import pandas as pd
+
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, PowerTransformer
+from sklearn.cluster import KMeans
+
+import preprocessing_permits as pr
+import numpy as np
+
 ############################################################################################################
 #                                        Evaluations                                                       #
 ############################################################################################################
